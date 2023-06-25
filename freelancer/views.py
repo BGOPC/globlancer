593f2503e0174cfa49ded178ef1c444746067e9c
BGOPC <bb8933400@gmail.com>
2023-06-25 16:38:53 +0330

    HomePage, views imports

--- freelancer/views.py
+++ freelancer/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
+
 
 # Create your views here.
+class HomeView(TemplateView):
+    template_name = "freelancer/home_page.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['user'] = self.request.user if str(self.request.user) != "AnonymousUser" else None
