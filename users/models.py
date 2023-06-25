30e600cea0a8fa5bac21d83e4689cf7a2a5c6c44
BGOPC <bb8933400@gmail.com>
2023-06-25 16:28:07 +0330

    renamed profile to profile_picture

--- users/models.py
+++ users/models.py
@@ -10,7 +10,7 @@ class User(AbstractUser):
     password = models.CharField(max_length=255, null=True)
     email = models.EmailField(max_length=255, unique=True)
     phoneNum = PhoneNumberField(null=False, unique=True, default='')
-    profile = models.ImageField(upload_to="profile/", null=True, blank=True,
+    profile_picture = models.ImageField(upload_to="profile/", null=True, blank=True,
                                 default="https://www.seekpng.com/png/detail/413-4139803_unknown-profile-profile-picture-unknown.png")
     USERNAME_FIELD = "username"
     REQUIRED_FIELDS = ["email", "first_name", "last_name", "password"]
