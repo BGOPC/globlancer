01cd65faa81aa201f40e1f309217dd141f37daab
BGOPC <bb8933400@gmail.com>
2023-06-25 16:29:40 +0330

    Linted

--- users/models.py
+++ users/models.py
@@ -2,6 +2,8 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
 
+default_profile_picture = "https://www.seekpng.com/png/detail/413-4139803_unknown-profile-profile-picture-unknown.png"
+
 
 # Create your models here.
 class User(AbstractUser):
@@ -11,7 +13,7 @@ class User(AbstractUser):
     email = models.EmailField(max_length=255, unique=True)
     phoneNum = PhoneNumberField(null=False, unique=True, default='')
     profile_picture = models.ImageField(upload_to="profile/", null=True, blank=True,
-                                default="https://www.seekpng.com/png/detail/413-4139803_unknown-profile-profile-picture-unknown.png")
+                                        default=default_profile_picture)
     USERNAME_FIELD = "username"
     REQUIRED_FIELDS = ["email", "first_name", "last_name", "password"]
     is_freelancer = False
