77834af63b6f5b4e90f3968b9593057cc8a137a1
BGOPC <bb8933400@gmail.com>
2023-06-26 13:07:32 +0330

    About is temporary the home page

--- freelancer/urls.py
+++ freelancer/urls.py
@@ -3,14 +3,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.HomeView.as_view()),
+    # path("", views.HomeView.as_view()),
+    path("", views.AboutView.as_view()), # Temporary
     path("freelancers/", views.FreeLancersListView.as_view(), name="FreeLancersList"),
     path("freelancer/<str:username>", views.FreeLancerView.as_view(), name="FreeLancerList"),
     path("apply", views.ApplyView.as_view(), name="Apply"),
     path("new", views.NewProjectView.as_view(), name="NewProject"),
     path("project/<int:projectID>", views.ProjectDetailView.as_view(), name="Project"),
     path("projects/", views.ProjectsListView.as_view(), name="ProjectsList"),
-    path("about/", views.AboutView.as_view(), name="About"),
+    # path("about/", views.AboutView.as_view(), name="About"),
     path("contact/", views.ContactView.as_view(), name="Contact"),
     path('switch-language/<str:lang_code>/', views.switch_language, name='switch_language'),
 ]
